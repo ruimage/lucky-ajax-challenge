d6ee151353586474db805078f490c8173d3170cc
abakharev <abakharev@sharevault.com>
2022-05-19 13:21:16 +0300

    replace hbs with react

--- views/Error.jsx
+++ views/Error.jsx
@@ -1,7 +1,7 @@
 const React = require('react');
 const Layout = require('./Layout');
 
-module.exports = function Main({ title, message, error }) {
+module.exports = function Error({ title, message, error }) {
   return (
     <Layout title={title}>
       <h1>{message}</h1>
